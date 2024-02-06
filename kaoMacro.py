bd0e4217b19933dcd1f76a1f5b6b7aa32b13c961
Eatheun <ethan.al469@gmail.com>
2024-02-06 11:55:53 +1100

    Cleaned Up Code and Adjusted Metrics

--- kaoMacro.py
+++ kaoMacro.py
@@ -20,12 +20,10 @@ def copyKaoClpbrd(category, kaoType):
     # concatenating the giant relative path
     filepath = "./All Kaomojis" + "/" + category + "/" + kaoType + ".txt";
     with open(filepath, "r", encoding = "utf-8") as kaoFile:
-        # find and grab the kaomoji
+        # find the Kaomoji, copy to clipboard, print for debugging
         kaomoji = "";
         for i in range(random.randrange(kaoNum)): kaomoji = kaoFile.readline();
         kaomoji = kaomoji.removesuffix("\n");
-        
-        # copy to clipboard and print for debugging
         clpbrd.copy(kaomoji);
         print(kaomoji);
         
@@ -37,17 +35,17 @@ def copyKaoClpbrd(category, kaoType):
 #### CONSTANTS ####
 
 # canvas metrics
-circOutR = int(min(320, pag.size()[1] / 4.5)); # outer radius of canvas circle, limited by screen res
-circInR = int(min(80, circOutR / 2)); # inner radius of canvas circle cutout, limited by circOutR
-c = circOutR; # centre of circle
-circAngleOffset = 6; # gap between each polygon in the canvas circle
+outR = int(min(320, pag.size()[1] / 4.5)); # outer radius of canvas circle, limited by screen res
+inR = int(min(80, outR / 2)); # inner radius of canvas circle cutout, limited by outR
+c = outR; # centre of circle
+circAngOff = 5; # gap between each polygon in the canvas circle
 
 # text
 myFont = "Comic Sans MS Bold"; # title font
 fillCol = "turquoise4"; # fill colour of canvas polygons
 outlineCol = "LightSteelBlue1"; # outline colour of canvas polygons
 transCol = "#add123"; # transparent colour lol
-baseFontSize = circOutR / 18;
+baseFontSize = outR / 18;
 
 # tkinter
 bordWidth = 3; # border width of the canvas
@@ -56,15 +54,18 @@ winTrans = 0.3; # base transparency of window
 steps = 5; # general steps to increment
 
 # display port metrics
-padding = 10; # general padding
-canvH = circOutR * 2; # height of canvas
-canvW = circOutR * 2; # width of canvas
-winSize = f'{(canvW + (padding + bordWidth) * 2) * 2}x{canvH + 216}'; # window size
+pad = 10; # general padding
+canvH = outR * 2; # height of canvas
+canvW = outR * 2; # width of canvas
+winSize = f'{(canvW + (pad + bordWidth) * 2) * 2}x{canvH + 216}'; # window size
 
-# Helper functions
+#### GLOBAL VARIABLES ####
+
 lastClickX = 0;
 lastClickY = 0;
-publicWindow = "";
+publicWindow = ""; # gets assigned when a new KaoApp() is instanced
+
+# Helper functions
 def saveLastClickPos(event):
     global lastClickX, lastClickY;
     lastClickX = event.x;
@@ -82,11 +83,17 @@ def fade(isIn, window):
         window.attributes("-alpha", start + (1 if isIn else -1) * (i + 1) * fadeIncr);
         time.sleep(0.02);
 
+def calcCircXY(r, a):
+    return (
+        c + r * cos(a) + pad, # X
+        outR + r * sin(a) + pad # Y
+    );
+
 def polygonDoSomething(event):
-    x = event.x - c - padding;
-    y = event.y - circOutR - padding;
+    x = event.x - c - pad;
+    y = event.y - outR - pad;
     dist = sqrt((x ** 2) + (y ** 2));
-    if circInR <= dist and dist <= circOutR:
+    if inR <= dist and dist <= outR:
         print(f'{(x, y)}');
 
 ################################ MAIN WINDOW ################################
@@ -106,19 +113,25 @@ class KaoApp(Tk):
         # top
         self.topFrame = TopSection(self);
         top = self.topFrame;
+        top.bindHover(self);
+        
         top.closeButton.assignQuit(self);
         top.title.bindDragWin();
         
         # middle
-        self.inCate = StringVar(value = "");
-        self.inType = StringVar(value = "");
         self.middleFrame = MiddleSection(self);
         middle = self.middleFrame;
+        middle.bindHover(self);
+        
+        self.inCate = StringVar(value = "");
+        self.inType = StringVar(value = "");
         middle.cateFrame.cateComb.assignStrVar(self.inCate);
         middle.typeFrame.typeComb.assignStrVar(self.inType);
         
         # bottom
         self.bottomFrame = BottomSection(self);
+        bottom = self.bottomFrame;
+        bottom.bindHover(self);
         
         self.mainloop();
     
@@ -127,7 +140,6 @@ class KaoApp(Tk):
         self.title("顔ウィール");
         self.minsize(width = 640, height = 480);
         self.geometry(winSize);
-        self.iconbitmap("icon.ico");
     
     # assigns window attributes
     def assignWinAttr(self):
@@ -145,11 +157,9 @@ class KaoApp(Tk):
 class Section(Frame):
     def __init__(self, master):
         super().__init__(master);
-        self.pack(pady = padding);
+        self.pack(pady = pad);
         
-        self.bindHover(master);
-
-    # binds hovering to the section
+    # binds window hovering to the section
     def bindHover(self, window):
         self.bind("<Enter>", lambda event: fade(True, window));
         self.bind("<Leave>", lambda event: fade(False, window));
@@ -186,12 +196,12 @@ class Title(Label):
 class MiddleSection(Section):
     def __init__(self, master):
         super().__init__(master);
-        self.pack(pady = padding);
+        self.pack(pady = pad);
         
         # draws the initial frames and the category circle
         self.cateFrame = CateFrame(self);
         self.typeFrame = TypeFrame(self);
-        self.cateFrame.cateCirc.drawCircle(len((list(all.keys()))), circAngleOffset, list(all.keys()));
+        self.cateFrame.cateCirc.drawCircle(len((list(all.keys()))), circAngOff, list(all.keys()));
         self.bindDrawCircle();
     
     # sets values of the types combobox based on the category selected
@@ -212,7 +222,7 @@ class MiddleSection(Section):
             ),
             lambda event: typeCircRef.drawCircle(
                 len(typeCombRef["values"]),
-                circAngleOffset,
+                circAngOff,
                 typeCombRef["values"]
             )
         ];
@@ -246,22 +256,18 @@ class TypeFrame(SubCircleSection):
         self.typeComb = Combo(self);
 
 class Circle(Canvas):
-    def __init__(self, master, w = canvW + padding * 2, h = canvH + padding * 2, bdw = bordWidth, rlf = "raised"):
+    def __init__(self, master, w = canvW + pad * 2, h = canvH + pad * 2, bdw = bordWidth, rlf = "raised"):
         super().__init__(master, width = w, height = h, borderwidth = bdw, relief = rlf);
-        self.pack(pady = padding);
+        self.pack(pady = pad);
     
         self.bindClickCanvas();
     
     # draw filled polygons
     def drawPolygon(self, angOrd, radOrd):
         polyCorners = list(map(
-				lambda i: (
-					c + radOrd[i] * cos(angOrd[i]) + padding,
-                	circOutR + radOrd[i] * sin(angOrd[i]) + padding
-				),
-				range(4)
-            )
-        );
+            lambda i: calcCircXY(radOrd[i], angOrd[i]),
+            range(4)
+        ));
         
         polyPoints = [];
         for i in range(4):
@@ -271,10 +277,7 @@ class Circle(Canvas):
                 a0 = angOrd[i]; a1 = angOrd[i + 1];
                 step = (a1 - a0) / angSteps;
                 polyPoints.append(list(map(
-					lambda j: (
-						c + radOrd[i] * cos(a0 + j * step) + padding,
-						circOutR + radOrd[i] * sin(a0 + j * step) + padding
-					),
+					lambda j: calcCircXY(radOrd[i], a0 + j * step),
 					range(angSteps)
 				)));
         
@@ -292,47 +295,39 @@ class Circle(Canvas):
         if pcs == 0: return;
         self.delete("polygon", "centre"); # clears all the circles before
         
-        # basic metrics
+        # offset metrics
         inOff = radians(min(angleOff, 180 / pcs));
-        outOff = atan((circInR / circOutR) * (tan(inOff)));
+        outOff = atan((inR / outR) * (tan(inOff)));
         
         # loop through angles as cosi + isini
         angles = np.linspace(90, 450, pcs + 1);
         for i in range(pcs):
+            # current and next angle of each polygon
             curr = radians(angles[i]);
             next = curr + radians(360 / pcs);
-            angOrd = [
-                curr + outOff,
-                next - outOff,
-                next - inOff,
-                curr + inOff
-            ]; # order of the angles for each point
-            radOrd = [
-				circOutR,
-				circOutR,
-				circInR,
-				circInR,
-			]; # order of the radii for the corresponding angles
+            
+            # order angles and radii for each corresponding point, with offsets
+            angOrd = [curr + outOff, next - outOff, next - inOff, curr + inOff];
+            radOrd = [outR, outR, inR, inR];
             
             # draw fill first
             self.drawPolygon(angOrd, radOrd);
 
             # overlaying current value as text
-            midRad = (circOutR + circInR) / 2;
+            midRad = (outR + inR) / 2;
             midAng = (curr + next) / 2;
             tag = values[i].replace(" ", "\n");
             self.create_text(
-                c + midRad * cos(midAng) + padding,
-                circOutR + midRad * sin(midAng) + padding,
+                calcCircXY(midRad, midAng),
                 font = (myFont, floor((4 / pcs) * baseFontSize)), # scale the font down, 0 - 90 degrees
                 text = tag,
                 tag = ("polygon")
             );
             
         # draw centre self
-        offSpace = sqrt(2 * (circInR ** 2) * (1 - cos(radians(2 * angleOff))));
-        cCircOff = circInR - offSpace;
-        p1, p2 = c - cCircOff + padding, c + cCircOff + padding
+        offSpace = sqrt(2 * (inR ** 2) * (1 - cos(radians(2 * angleOff))));
+        cCircOff = inR - offSpace;
+        p1, p2 = c - cCircOff + pad, c + cCircOff + pad
         self.create_oval(
             [(p1, p1), (p2, p2)],
             fill = fillCol,
@@ -348,7 +343,7 @@ class Circle(Canvas):
 class Combo(widg.Combobox):
     def __init__(self, master):
         super().__init__(master, font = (myFont, 8), width = 26, state = "readonly");
-        self.pack(pady = padding);
+        self.pack(pady = pad);
 
     # assigns a string variable to the combobox
     def assignStrVar(self, strVar):
